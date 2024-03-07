e45408902eedba366d8bb9e519669998d4bb0e8b
KuengWD08 <03230140.gcbs@rub.edu.bt>
2024-03-07 15:00:37 +0600

    strings application

---
+++ strings.py
@@ -0,0 +1,40 @@
+# string manipulation
+
+#string concanetation
+string = 'Hello'
+string2 = 'World'
+
+combined = string +" "+ string2
+combined1 = 'HI'+","+" "+"there"
+
+print("string concanetation")
+print(combined)
+print(combined1)
+
+# string interpolation
+name ='John'
+age = '32'
+interpolated = f"John is {age} years of age"
+print(interpolated)
+# how do you print "john is 30 years of age"
+
+# ! string methods
+print('String Methods')
+# # ! upper
+print('String upper')
+stringlower = 'hello how are you'
+converted =  stringlower.upper()
+converted1 = "pure string".upper()
+
+#  lower
+print('String lower')
+lower = "HELLO".lower()
+print(lower)
+
+# # ! capitalize
+print('String capitalize')
+capitalize = "hello there".capitalize()
+print(capitalize)
+
+
+
