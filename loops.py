44442651066dd6c200ea2e36b6cdb31d586788ab
KuengWD08 <03230140.gcbs@rub.edu.bt>
2024-03-14 14:56:00 +0600

    loops

---
+++ loops.py
@@ -0,0 +1,32 @@
+#loops over an array
+fruits = ['a','b','c']
+#loops through each elements
+#at each stage, if the elements is 'a'
+#print True
+for e in fruits:
+    if e == 'a':
+        print('True')
+    if e == 'b':
+        print('True')
+
+#iterate over a string
+greetings = 'hello'
+for char in greetings:
+    print(char)
+
+#Exercise : check if strings contains vowel
+    
+#iterate over a range
+    for i in range(20):
+        print(10)
+#20 = no of iterations
+
+for i in range(9):
+    val = i + 5 # 5,6,7,8,9,10,11,12,13
+    print(val)
+
+for i in range(5,14):
+    print(i)
+
+for i in range(5,14,2):
+    print(i)
