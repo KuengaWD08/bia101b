591417a1c557a006da5ff037f61394c544e9ecbd
KuengWD08 <03230140.gcbs@rub.edu.bt>
2024-03-14 15:21:17 +0600

    functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+
+def greet():
+    #do some calculations code
+    #complex stuff
+    print('Hello world')
+
+greet()
+
+def add_numbers(num1,num2):
+    sum = num1 + num2
+    print('adding numbers')
+    return sum
+
+userNumber = 10
+userNumber2 = 20
+
+returnsum=add_numbers(userNumber,userNumber2)
+print(returnsum)
