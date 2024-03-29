e6f931903089700811cadec41b74821703110804
KuengWD08 <03230140.gcbs@rub.edu.bt>
2024-03-29 12:20:59 +0600

    contains Duplicate

---
+++ linear.py
@@ -0,0 +1,14 @@
+#searching
+#sorting
+
+# Problem 1
+# input
+user_input = [1,2,3,4,5,6,7,8,9,11]
+
+# check to see if a certain number exist in the user input array
+n = 14
+
+for i in user_input:
+    if n == i:
+        print('exist')
+print('not found')
